b1c41f02b7e616847e9255f4cb2226f1f7bd2622
NwoluDavid <nwoludave@gmail.com>
2024-07-22 18:46:24 +0100

    updated project models and schemas

--- src/app/project/models.py
+++ src/app/project/models.py
@@ -15,14 +15,14 @@ def datetime_now_sec():
 class Project(Base):
     name : str =Field(default = None, min_length =8)
     address: str=Field(default = None , min_length =8)
-    zipcode : int =Field(default =None, gt =5)
+    zipcode : Optional[int] =Field(default =None, gt =5)
     created: Optional[datetime] = Field(default_factory=datetime_now_sec)
     modified: Optional[datetime] = Field(default_factory=datetime_now_sec)
     amount: int
     duration: datetime=Field(default_factory=datetime_now_sec)
     title: str =Field(default = None, min_length =8)
-    about:str =Field(default = None, min_length =8)
+    about:Optional[str] =Field(default = None, min_length =8)
     picture_or_video: str
     categories: str
-    story:str
+    story:Optional[str]=Field(default =None)
     user_id : ObjectId
