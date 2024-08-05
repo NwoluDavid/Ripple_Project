c8067cfed3804e818e2f4e52b766c918fc6b71f1
NwoluDavid <nwoludave@gmail.com>
2024-08-05 14:52:58 +0100

    payment setting out logic

---
+++ src/app/payment/router.py
@@ -0,0 +1,25 @@
+@router.post("/verify-transaction{ref_id}")
+async def verify_transaction(ref_id: str,  db: Annotated[Session, Depends(get_db)]):
+    "Verify the transaction id and provision the user"
+    payment_status,data = verify_payment(ref_id)
+    if payment_status == True:
+    # booking = db.query(Waste).find(Waste.id == ref_id)
+        booking = db.get(Waste, int(ref_id))
+        booking.order_status = BookingStatus.delivered
+        
+        
+    for field, value in booking.dict().items():
+        setattr(booking, field, value)
+    
+    db.commit()
+    db.refresh(booking)
+    
+    booking = jsonable_encoder(booking)
+    
+    return JSONResponse(
+    status_code=201,
+    content={
+        "message": "payment successfully",
+        "booking":booking
+    })
+       
